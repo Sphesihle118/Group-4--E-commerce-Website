c158de5172ef0810db99d67daec0c904aefbb8aa
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-11-16 01:11:27 +0200

    Added the product image attribute

--- src/data/productDetails.js
+++ src/data/productDetails.js
@@ -3,14 +3,16 @@ export const productDetails = [
         product: 1,
         name: "Apple Watch",
         smallDescription: "Series 5 SE",
+        productIMG: "./src/assets/Applewatch.png",
         price: "$529.99",
         fullDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero."
-    }
+    },
 
     {
         product: 2,
         name: "Sony ZX330BT",
         smallDescription: "Light Grey",
+        productIMG: "./src/assets/Applewatch.png",
         price: "$39.99",
         fullDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero."
   
