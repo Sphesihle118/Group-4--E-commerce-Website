3ab3089bc6b3d3a5ef37bb64a43783049baca662
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-11-16 01:03:54 +0200

    Entered product 1 data

---
+++ src/data/productDetails.js
@@ -0,0 +1,9 @@
+export const productDetails = [
+    {
+        product: 1,
+        name: "Apple Watch",
+        smallDescription: "Series 5 SE",
+        price: "$529.99",
+        fullDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero."
+    }
+]
