bcb94da939618181da7d7453197a776a23f06df6
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-11-17 00:48:14 +0200

    Added the rating attribute on product 1

--- src/data/productDetails.js
+++ src/data/productDetails.js
@@ -5,6 +5,7 @@ export const productDetails = [
         smallDescription: "Series 5 SE",
         productIMG: "./src/assets/Applewatch.png",
         price: "$529.99",
+        rating: "4.5/5",
         fullDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero."
     },
 
