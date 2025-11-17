f41c4de79d35de33ef7c24fca363ec083811ccfd
Tholakele-jiyana <tholakelejiyana@gmail.com>
2025-11-17 23:40:01 +0200

    Added the bagCart product details

---
+++ src/data/productDetails2.js
@@ -0,0 +1,44 @@
+export const productDetails2 = [
+    {
+        product: 9,
+        name: "Dell XPS 13",
+        smallDescription: "White",
+        productIMG: "./src/assets/DellXPS13.png",
+        price: "$1799.99",
+        rating: "4.5/5",
+        fullDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
+    },
+
+    {
+        product: 10,
+        name: "Iphone 11",
+        smallDescription: "Navy Blue",
+        productIMG: "./src/assets/Iphone11NavyBlue.png",
+        price: "$729.99",
+        rating: "4.5/5",
+        fullDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero."
+    },
+
+    {
+        product: 11,
+        name: "Iphone 11",
+        smallDescription: "Milky White",
+        productIMG: "./src/assets/Iphone11MilkyWhite.png",
+        rating: "4.5/5",
+        price: "$619.99",
+        fullDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero."
+  
+    },
+
+    {
+        product: 12,
+        name: "Iphone 11",
+        smallDescription: "Black",
+        productIMG: "./src/assets/Iphone11Black2.png",
+        rating: "4.5/5",
+        price: "$619.99",
+        fullDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero."
+  
+    },
+
+]
